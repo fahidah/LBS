79b7bd1c4cb94e2d88430069e60139678f26c6ec
fahidah <abdulsalamfaheedatopeyemi@gmail.com>
2020-10-20 17:20:59 +0100

    Add files via upload

---
+++ Assets/FurnishedCabin/Scripts/DisplayObjectName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayObjectName : MonoBehaviour
+{
+    public TextMeshProUGUI tagName;
+
+    public GameObject nameDisplay;
+
+    // Update is called once per frame
+    public void displayTagName()
+    {
+            nameDisplay.gameObject.SetActive(true);
+            tagName.text = "This is a " + gameObject.tag;
+    }
+
+}
